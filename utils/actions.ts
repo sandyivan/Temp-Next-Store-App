5a076b5fef4827b866581e03144f0b1ddf909b24
Sandeep Ivan Albarillo <zandalba@gmail.com>
2025-07-16 10:21:41 +0800

    refactor form component to be reusable one

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,26 @@
+"use server"
+
+import { auth, currentUser } from '@clerk/nextjs/server';
 import db from '@/utils/db'
 import { redirect } from 'next/navigation';
+
+
+// helper functions 
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  };
+};
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  return user;
+};
+
 
 
 export const fetchFeaturedProducts = async () => {
@@ -10,8 +31,6 @@ export const fetchFeaturedProducts = async () => {
     });
     return products
 };
-
-
 
 export const fetchAllProducts = ({search=''}:{search:string}) => {
     return db.product.findMany({
@@ -26,7 +45,6 @@ export const fetchAllProducts = ({search=''}:{search:string}) => {
         },
     });
 };
-
 
 // fetchign single product 
 export const fetchSingleProduct = async (productId:string) => {
@@ -46,3 +64,35 @@ export const fetchSingleProduct = async (productId:string) => {
 
 }
 
+
+// this must mathc the our type in utils/types.ts, its named - actionFunction
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const name = formData.get('name') as string;
+    const company = formData.get('company') as string;
+    const price = Number(formData.get('price') as string);
+    const image = formData.get('image') as File;
+    const description = formData.get('description') as string;
+    const featured = Boolean(formData.get('featured') as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: '/images/product-1.jpg',
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: 'product created' };
+  } catch (error) {
+    return renderError(error);
+  }
+};
